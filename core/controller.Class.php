2be3c46d3fa5f9e687fd8ef8111b7b81063c6961
derrick921213 <dlin12457@gmail.com>
2023-05-24 13:27:17 +0800

    test

--- core/controller.Class.php
+++ core/controller.Class.php
@@ -39,6 +39,35 @@ class Controller{
         }
         return $code;
     }
+    //print data
+    function printData(){
+        $user = $this->db->prepare("SELECT * FROM users ORDER BY id");
+        $user->execute();
+        $content = '
+            <table class="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Avatar</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>    
+        ';
+        while($userInfo = $user->fetch(PDO::FETCH_ASSOC)){
+            $content .= '
+                <tr>
+                    <td>'.$userInfo["f_name"].'</td>
+                    <td>'.$userInfo["l_name"].'</td>
+                    <td><img style="max-width: 50px;" src="'.$userInfo["avatar"].'" alt="avatar"></td>
+                    <td>'.$userInfo["email"].'</td>
+                </tr>
+            ';
+        }
+        $content .= '</tbody></table>';
+        return $content;
+    }
     //Init data
     function insertData($data){
         $checkUser = $this->db->prepare("SELECT * FROM users WHERE email=:email");
--- page/people.php
+++ page/people.php
@@ -4,12 +4,14 @@ require_once($_SESSION["Base"]);
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 $url = $URLS;
-Base(function() use ($url){
+Base(function($user,$passwd) use ($url){
+    $controller = new Controller($user,$passwd);
+    $url['user_table'] = $controller->printData(); 
     $loader = new FilesystemLoader(ROOT_PATH.'/templates');
     $twig = new Environment($loader);
     if (basename($_SERVER["REQUEST_URI"]) == basename(__FILE__)){
         $url["people_active"] = "active";
-        echo $twig->render('orders.twig',$url);
+        echo $twig->render('people.twig',$url);
     }
 });
 ?>
