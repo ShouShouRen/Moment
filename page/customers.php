5daa0974e82cc54b42156510e8465923f08ef129
derrick921213 <dlin12457@gmail.com>
2023-06-06 20:14:05 +0800

    test

--- page/customers.php
+++ page/customers.php
@@ -9,7 +9,11 @@ require_once($_SESSION["Base"]);
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 $url = $URLS;
-Base(function() use ($url){
+Base(function($user,$passwd) use ($url){
+    $db = new Connect($user,$passwd);
+    $sql = "SELECT *  FROM `Guests`";
+    $stmt = $db->prepare($sql);
+    $ret = $stmt->execute();
     $loader = new FilesystemLoader(ROOT_PATH.'/templates');
     $twig = new Environment($loader);
     if (basename($_SERVER["REQUEST_URI"]) == basename(__FILE__)){
